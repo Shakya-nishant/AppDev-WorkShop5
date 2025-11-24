05808902798207d2d232ebfe7af0697673fab0f5
Shakya-nishant <155061583+Nishant10011@users.noreply.github.com>
2025-11-24 11:27:29 +0545

    Task 1

---
+++ Task 1/BankAccount.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class BankAccount
+    {
+        private string accountNumber;
+        private double balance;
+
+        public string AccountNumber
+        {
+            get { return accountNumber; }
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+            private set
+            {
+                if (value > 0)
+                {
+                    balance = value;
+                }
+                else
+                {
+                    Console.WriteLine("Balance cannot be zero or negative!");
+                }
+            }
+        }
+
+        // Constructor to set account number and initial balance
+        public BankAccount(string accNumber, double initialBalance)
+        {
+            accountNumber = accNumber;
+            Balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"Deposited: {amount}");
+            }
+            else
+            {
+                Console.WriteLine("Deposit amount must be greater than 0!");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Withdraw amount must be greater than 0!");
+            }
+            else if (amount > balance)
+            {
+                Console.WriteLine("Insufficient balance!");
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine($"Withdrawn: {amount}");
+            }
+        }
+    }
+}
---
+++ Task 1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("123456789", 5000);
+
+            Console.WriteLine("Account Number: " + account.AccountNumber);
+            Console.WriteLine("Initial Balance: " + account.Balance);
+
+            account.Deposit(1500);
+            account.Withdraw(1000);
+
+            Console.WriteLine("Remaining Balance: " + account.Balance);
+        }
+    }
+
+}
