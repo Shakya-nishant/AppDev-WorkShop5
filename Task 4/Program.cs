4a9a62a0f97eceb56df65c74c98c800efdbf2bce
Shakya-nishant <155061583+Nishant10011@users.noreply.github.com>
2025-11-24 17:15:21 +0545

    Task 4

---
+++ Task 4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Bike bike = new Bike();
+
+            Console.WriteLine("Car Object:");
+            car.Display();
+            car.StartEngine();
+            car.StopEngine();
+
+            Console.WriteLine("-------------------------");
+
+            Console.WriteLine("Bike Object:");
+            bike.Display();
+            bike.StartEngine();
+            bike.StopEngine();
+
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Task 4/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    public abstract class Vehicle
+    {
+        public abstract void StartEngine();
+        public abstract void StopEngine();
+
+        public void Display()
+        {
+            Console.WriteLine("This is a vehicle");
+        }
+    }
+
+}
